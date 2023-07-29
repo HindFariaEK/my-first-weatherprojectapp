14b2553d2b25a7e4693e82ac68ecc357b89003c3
HindFariaEK <fariahind@gmail.com>
2023-07-30 00:14:34 +0200

    added the HTML  to Javascript for the forecast

--- src/index.js
+++ src/index.js
@@ -65,6 +65,31 @@ function lookforCity(event) {
 let cityForm = document.querySelector("#form");
 cityForm.addEventListener("submit", search);
 
+function displayForecast() {
+  let forecastElement = document.querySelector(".container");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `<div class="container">
+        <div class="col date-forecast">
+          <div class="weather-forecast-date">${day}</div>
+          <span class="icon">☀️</span>
+          <div class="weather-forecast">
+            <span class="weather-forecast-max">18°</span>
+
+            <span class="weather-forecast-min">13°</span>
+          </div>
+          <i class="fa-regular fa-eye fa-2xs eye-icon"></i>
+        </div>
+        </div>`;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
 function showWeather(response) {
   document.querySelector("#searchcity").innerHTML = response.data.name;
   document.querySelector("#temp").innerHTML = Math.round(
@@ -93,9 +118,10 @@ function search(event) {
 }
 
 function searchCity(city) {
-  let apiKey = "b2d9fa1f2b35557e4615dd5fab218834";
+  let apiKey = "b40b135798f82a05aed08769f9275f50";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showWeather);
 }
 
 searchCity("Tripoli");
+displayForecast();
